d22a9fcb64527b4a9ef3f965a49cbca9e245dc57
Yukichi Kawada <yukichikawada@gmail.com>
2018-04-23 11:10:05 -0700

    Change service type to be Observable of Hero array

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,17 +10,18 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes: Hero[];
 
-  heroes = HEROES;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+          .subscribe(heroes => this.heroes = heroes);
+  }
 
   selectedHero: Hero;
 
